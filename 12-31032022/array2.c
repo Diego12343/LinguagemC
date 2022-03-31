b04734fb6cbaad2c50f10ad30001271a17771db3
Diego Gomes <dg806282@gmail.com>
2022-03-31 12:07:22 -0300

    Array

--- 12-31032022/array2.c
+++ 12-31032022/array2.c
@@ -9,5 +9,9 @@ void main(){
     int v[10]={1,23,52,4,0,1,2,5,8,10};
     soma(v,10);
     maiorValor(v,10);
+    menorValor(v,10);
+}    
+
+
     
-}
+
--- 12-31032022/calculos.h
+++ 12-31032022/calculos.h
@@ -18,5 +18,30 @@ void maiorValor(int array[],int tamanho){
         }
     }
     printf("o maior valor é %d\n",m);
+}
 
+    void menorValor(int array[],int tamanho){
+    int m = array[0];
+    for(int i = 1;i <tamanho ; i++){
+        if(array[i]< m){
+            m = array[i];
+        }
+    }
+    printf("O menor valor é %d\n",m);
+
+}
+
+
+
+void ordenar(int valores[], int tamanho){
+
+    for(int j=1; j < tamanho; j++){
+        int x= valores[j];
+        int i;
+        for(i =j-1 ; i >= 0 && valores[1] > x; i--){
+            valores[i+1]= valores[i];
+        }
+        valores[i+1]= x;
+    }
+    for(int p=0 ; p < tamanho ; p++);
 }
